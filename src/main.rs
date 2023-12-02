b7167b21cd9af3719041562357288a62b283a0c3
Lilith Majorana Usami <mewtrino@mewtrino.org>
2023-12-02 18:59:36 -0500

    Day 2 and input file generalization

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use std::io::Read;
 use std::path::Path;
 
 fn main() {
-    day_1_part_2();
+    day_2();
+}
+
+fn day_input(day: u8) -> String {
+    let mut input_file = File::open(Path::new(&format!("input/day{}", day))).unwrap();
+    let mut input = String::new();
+    input_file.read_to_string(&mut input).unwrap();
+    input
 }
 
 fn day_1_part_1() {
-    let mut input_file = File::open(Path::new("input/day1"))
-        .expect("Failed to open day 1 file");
-    let mut input = String::new();
-    input_file.read_to_string(&mut input).expect("Failed to read day 1 file");
+    let input = day_input(1);
     let mut sum = 0;
     for line in input.lines() {
         let mut nums = Vec::new();
@@ -28,10 +32,7 @@ fn day_1_part_1() {
 }
 
 fn day_1_part_2() {
-    let mut input_file = File::open(Path::new("input/day1"))
-        .expect("Failed to open day 1 file");
-    let mut input = String::new();
-    input_file.read_to_string(&mut input).expect("Failed to read day 1 file");
+    let input = day_input(1);
     let mut sum = 0;
     let digits = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
     for line in input.lines() {
@@ -53,4 +54,47 @@ fn day_1_part_2() {
         sum += num.parse::<i32>().unwrap();
     }
     println!("Day 1 Part 2:{}", sum);
+}
+
+fn day_2() {
+    let games = day_input(2);
+    let mut valid_game_sum = 0;
+    let mut power_sum = 0;
+    let (max_red, max_green, max_blue) = (12, 13, 14);
+    for (game_n, game) in games.lines().enumerate() {
+        let (mut actual_red, mut actual_green, mut actual_blue) = (0, 0, 0);
+        if let Some((_, game)) = game.split_once(": ") {
+            for sub_game in game.split("; ") {
+                for color_num in sub_game.split(", ") {
+                    if let Some((num, color)) = color_num.split_once(" ") {
+                        let num = num.parse::<i32>().unwrap();
+                        match color {
+                            "red" => {
+                                if num > actual_red {
+                                    actual_red = num;
+                                }
+                            },
+                            "green" => {
+                                if num > actual_green {
+                                    actual_green = num;
+                                }
+                            },
+                            "blue" => {
+                                if num > actual_blue {
+                                    actual_blue = num;
+                                }
+                            },
+                            _ => (),
+                        }
+                    }
+                }
+            }
+        }
+        if max_red >= actual_red && max_green >= actual_green && max_blue >= actual_blue {
+            valid_game_sum += game_n + 1;
+        }
+        power_sum += actual_red * actual_green * actual_blue;
+    }
+    println!("Day 2 Part 1:{}", valid_game_sum);
+    println!("Day 2 Part 2:{}", power_sum);
 }
